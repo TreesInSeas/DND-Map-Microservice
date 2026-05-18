5e26dbdce5c98e1abdfdee8b0313c7acba38886f
marcosotelo12 <soteloma@oregonstate.edu>
2026-05-17 18:15:24 -0700

    client_example.py

--- client_example.py
+++ client_example.py
@@ -1,6 +1,6 @@
 import requests
 print("Testing get map by ID...")
-response = requests.get(""http://127.0.0.1:5002/api/maps/test-map-id")
+response = requests.get("http://127.0.0.1:5002/api/maps/test-map-id")
 
 print(response.json())
 print("\nTesting get maps in room...")
